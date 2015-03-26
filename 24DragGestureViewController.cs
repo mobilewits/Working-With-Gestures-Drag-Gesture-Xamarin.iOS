ccba05d5aa3825d99425dacd56630eb193b8c79e
Mobilewits Mobile apps developers <harry@Harrys-Mac-Pro.local>
2015-03-26 16:19:24 -0400

    Working with Drag Gesture Xamarin.iOS

---
+++ 24DragGestureViewController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace DragGesture
+{
+	public partial class _4DragGestureViewController : UIViewController
+	{
+
+
+		UIRotationGestureRecognizer rotateGesture;
+		UIPanGestureRecognizer panGesture;
+		UIImageView imageView
+
+
+		public _4DragGestureViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+
+
+			float r = 0;
+			float dx = 0;
+			float dy = 0;
+
+
+			using (var image = UIImage.FromFile ("img/Finger.png")) {
+				img_view = new UIImageView (image){Frame = new RectangleF (new PointF(0,0), image.Size)};
+				img_view.UserInteractionEnabled = true;
+				View.AddSubview (img_view);
+			}
+
+
+			rotateGesture = new UIRotationGestureRecognizer ((rg) => {
+				if ((rg.State == UIGestureRecognizerState.Began || rg.State == UIGestureRecognizerState.Changed) && (rg.NumberOfTouches == 2)) {
+
+					img_view.Transform = CGAffineTransform.MakeRotation (rg.Rotation + r);
+					img_view.Transform=  
+				} else if (rg.State == UIGestureRecognizerState.Ended) {
+					r += rg.Rotation;
+				}
+			});
+
+
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+	}
+}
+
---
+++ 24DragGestureViewController.designer.cs
@@ -0,0 +1,29 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace DragGesture
+{
+	[Register ("_4DragGestureViewController")]
+	partial class _4DragGestureViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIImageView img_view { get; set; }
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (img_view != null) {
+				img_view.Dispose ();
+				img_view = null;
+			}
+		}
+	}
+}
